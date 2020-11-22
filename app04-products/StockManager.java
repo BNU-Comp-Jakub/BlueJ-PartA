d5a83124d556a5f3b9a934d0f16e6f27be721cd2
Kabudzio <kubawajerski@gmail.com>
2020-11-22 06:37:05 +0100

    update package

--- app04-products/StockManager.java
+++ app04-products/StockManager.java
@@ -42,6 +42,7 @@ public class StockManager
         if(id == stock.get(N).getID())
             {
                  stock.get(N).getQuantity();
+                
             }
         }        
     }
@@ -88,6 +89,5 @@ public class StockManager
         stock.get(id).getName();
         System.out.println(stock.get(id).getName());
         System.out.println(stock.get(id).getID());
-        System.out.println(stock.get(id).getQuantity());
     }
 }
